c18318a2fa173be2f0f560d353d8e977837ec9b5
O <chavitak@gmail.com>
2019-03-18 13:44:48 +0900

    modified add page

--- nuxt-auth/pages/album/add-album-review.vue
+++ nuxt-auth/pages/album/add-album-review.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">add Index</h1>
+      <h1 class="title" align="center">add Index</h1>
       <div>
         
     <v-container>
       <v-layout row wrap>
         <v-flex
         xs12
-        class="pa-2">
+        class="px-2 py-0">
           <v-text-field
             label="Url"
           ></v-text-field>
@@ -16,67 +16,93 @@
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="firstname"
-            label="First name"
+            v-model="album_name"
+            label="앨범명"            
           ></v-text-field>
         </v-flex>
 
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="lastname"
-            label="Last name"
+            v-model="artist_name"
+            label="아티스트"
           ></v-text-field>
         </v-flex>
 
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="email"
-            label="E-mail"
+            v-model="genre"
+            label="장르"
           ></v-text-field>
         </v-flex>
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="firstname"
-            label="First name"
+            v-model="released_date"
+            label="발매일"
           ></v-text-field>
         </v-flex>
 
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="lastname"
-            label="Last name"
+            v-model="label"
+            label="발매사"
           ></v-text-field>
         </v-flex>
 
         <v-flex
           xs12
           md4
-          class="pa-2"
+          class="px-2 py-0"
         >
           <v-text-field
-            v-model="email"
-            label="E-mail"
+            v-model="agency"
+            label="기획사"
           ></v-text-field>
         </v-flex>
+        <v-flex
+          xs12
+          md4
+          class="px-2 py-0"
+        >
+          <v-rating
+          v-model="rating"
+          color="yellow darken-3"
+          background-color="grey darken-1"
+          empty-icon="$vuetify.icons.ratingFull"
+          half-increments
+          hover
+        ></v-rating>
+        </v-flex>
+        <v-flex 
+        class="px-2 py-0"
+        xs12>
+        <v-textarea
+          outline
+          name="review"
+          label="리뷰"
+          value=""
+          hint="작성해주세요."
+        ></v-textarea>
+        </v-flex>
+      
       </v-layout>
     </v-container>
       </div>
