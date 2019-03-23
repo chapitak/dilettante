73258637370f848871a8e97a55b51f01f24749bf
chapitak <43088225+chapitak@users.noreply.github.com>
2019-03-23 22:53:55 +0900

    npm 꼬인듯 request 잘못임

--- nuxt-auth/nuxt.config.js
+++ nuxt-auth/nuxt.config.js
@@ -1,66 +1,67 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'dilettante',
+    title: "dilettante",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-	  {
-			//rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css' 
-		}
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        //rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
+      }
     ]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   },
-  modules: [
-  '@nuxtjs/axios',
-  '@nuxtjs/auth',
-  '@nuxtjs/vuetify'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth", "@nuxtjs/vuetify"],
   axios: {
-    baseURL: 'http://jeongkyo.kim:1337/'
+    baseURL: "http://jeongkyo.kim:1337/"
   },
 
   auth: {
     strategies: {
       local: {
         endpoints: {
-          login: { url: 'auth/local', method: 'post', propertyName: 'jwt' },
-          user: { url: 'users/me', method: 'get', propertyName: '' },
+          login: { url: "auth/local", method: "post", propertyName: "jwt" },
+          user: { url: "users/me", method: "get", propertyName: "" },
           logout: false
         }
       }
     }
   },
+  node: {
+    console: true,
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
   vuetify: {
     // Vuetify options
     //  theme: { }
   }
-}
-
+};
--- nuxt-auth/pages/album/add-album-review.vue
+++ nuxt-auth/pages/album/add-album-review.vue
@@ -1,189 +1,141 @@
+
+
 <template>
   <section class="section">
     <div class="container">
       <h1 class="title" align="center">add Index</h1>
       <div>
-        
-    <v-container>
-    <form method="post" @submit.prevent="submit">
-      <v-layout row wrap>
-        <v-flex
-        xs12
-        class="px-2 py-0">
-          <v-text-field
-            label="Url"
-            v-model="url"
-          ></v-text-field>
-         </v-flex>
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="album_name"
-            label="앨범명"            
-          ></v-text-field>
-        </v-flex>
+        <v-container>
+          <form method="post" @submit.prevent="submit">
+            <v-layout row wrap>
+              <v-flex xs12 class="px-2 py-0">
+                <v-text-field label="Url" v-model="url"></v-text-field>
+              </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="album_name" label="앨범명"></v-text-field>
+              </v-flex>
 
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="artist_name"
-            label="아티스트"
-          ></v-text-field>
-        </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="artist_name" label="아티스트"></v-text-field>
+              </v-flex>
 
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="genre"
-            label="장르"
-          ></v-text-field>
-        </v-flex>
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="released_date"
-            label="발매일"
-          ></v-text-field>
-        </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="genre" label="장르"></v-text-field>
+              </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="released_date" label="발매일"></v-text-field>
+              </v-flex>
 
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="label"
-            label="발매사"
-          ></v-text-field>
-        </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="label" label="발매사"></v-text-field>
+              </v-flex>
 
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-text-field
-            v-model="agency"
-            label="기획사"
-          ></v-text-field>
-        </v-flex>
-        <v-flex
-          xs12
-          md4
-          class="px-2 py-0"
-        >
-          <v-rating
-          v-model="rating"
-          color="yellow darken-3"
-          background-color="grey darken-1"
-          empty-icon="$vuetify.icons.ratingFull"
-          half-increments
-          hover
-        ></v-rating>
-        </v-flex>
-        <v-flex 
-        class="px-2 py-0"
-        xs12>
-        <v-textarea
-          outline
-          name="review"
-          label="리뷰"
-          value=""
-          v-model="review"
-          hint="작성해주세요."
-        ></v-textarea>
-        </v-flex>
-      </v-layout>
-      <div class="control">
-            <v-btn type="submit" style="display:block;float:right;">Register</v-btn>
-          </div>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-text-field v-model="agency" label="기획사"></v-text-field>
+              </v-flex>
+              <v-flex xs12 md4 class="px-2 py-0">
+                <v-rating
+                  v-model="rating"
+                  color="yellow darken-3"
+                  background-color="grey darken-1"
+                  empty-icon="$vuetify.icons.ratingFull"
+                  half-increments
+                  hover
+                ></v-rating>
+              </v-flex>
+              <v-flex class="px-2 py-0" xs12>
+                <v-textarea outline name="review" label="리뷰" value v-model="review" hint="작성해주세요."></v-textarea>
+              </v-flex>
+            </v-layout>
+            <div class="control">
+              <v-btn type="submit" style="display:block;float:right;">Register</v-btn>
+            </div>
           </form>
-    </v-container>
+        </v-container>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+const request = require("request");
+const cheerio = require("cheerio");
 
 export default {
   data() {
     return {
-      url: '',
-      album_id: '',
-      album_name: '',
-      artist_name: '',
-      genre: '',
-      released_date: '',
-      label: '',
-      agency: '',
+      url: "",
+      album_id: "",
+      album_name: "",
+      artist_name: "",
+      genre: "",
+      released_date: "",
+      label: "",
+      agency: "",
       rating: null,
-      review: ''
+      review: ""
+    };
+  },
+  watch: {
+    url: function() {
+      request(url, (error, response, body) => {
+        console.log(response);
+      });
+      console.log("잘됌.");
     }
   },
   methods: {
     submit() {
-      this.create_album_info()
-      this.create_review()
+      this.create_album_info();
+      this.create_review();
     },
     create_review() {
-        this.$axios.post(`http://jeongkyo.kim:1337/reviews/`, {
-              album_id: this.album_id,
-              rating: this.rating,
-              review_title: null,
-              review_text: this.review,
+      this.$axios
+        .post(`http://jeongkyo.kim:1337/reviews/`, {
+          album_id: this.album_id,
+          rating: this.rating,
+          review_title: null,
+          review_text: this.review
         })
         .then(response => {
-            // Handle success.
-            console.log(
-            'Well done, your post has been successfully created: ',
+          // Handle success.
+          console.log(
+            "Well done, your post has been successfully created: ",
             response.data
-            );
+          );
         })
         .catch(error => {
-            // Handle error.
-            console.log('An error occurred:', error);
+          // Handle error.
+          console.log("An error occurred:", error);
         });
     },
     create_album_info() {
-        this.$axios.post(`http://jeongkyo.kim:1337/albums/`, {
-            album_id: this.album_id,
-            album_name: this.album_name,
-            artist_id: this.artist_id,
-            artist_name: this.artist_name,
-            thumbnail: this.thumbnail,
-            url: this.url,
-            released_date: this.released_date,
-            genre: this.genre,
-            label: this.label,
-            agency: this.agency,
-            created_user: this.created_user
+      this.$axios
+        .post(`http://jeongkyo.kim:1337/albums/`, {
+          album_id: this.album_id,
+          album_name: this.album_name,
+          artist_id: this.artist_id,
+          artist_name: this.artist_name,
+          thumbnail: this.thumbnail,
+          url: this.url,
+          released_date: this.released_date,
+          genre: this.genre,
+          label: this.label,
+          agency: this.agency,
+          created_user: this.created_user
         })
         .then(response => {
-            // Handle success.
-            console.log(
-            'Well done, your post has been successfully created: ',
+          // Handle success.
+          console.log(
+            "Well done, your post has been successfully created: ",
             response.data
-            );
+          );
         })
         .catch(error => {
-            // Handle error.
-            console.log('An error occurred:', error);
+          // Handle error.
+          console.log("An error occurred:", error);
         });
     }
-
   }
-}
+};
 </script>
