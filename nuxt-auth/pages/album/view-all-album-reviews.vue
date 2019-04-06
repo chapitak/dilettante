a37b53a32f5b6b1cb68d13961795d7569367f8a7
chapitak <43088225+chapitak@users.noreply.github.com>
2019-04-06 09:03:32 +0900

    filter 완성

--- nuxt-auth/pages/album/view-all-album-reviews.vue
+++ nuxt-auth/pages/album/view-all-album-reviews.vue
@@ -3,23 +3,19 @@
     <v-layout row wrap>
       <v-spacer></v-spacer>
       <v-flex xs2>
-      <v-select
-          v-model="search_filter_type"
-          :items="filter_type"
-          label="filter"
-      ></v-select>
+        <v-select v-model="search_filter_type" :items="filter_type" label="filter"></v-select>
       </v-flex>
       <v-flex xs6>
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        hide-details
-        @input="filterSearch"
-      ></v-text-field>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          hide-details
+          @input="filterSearch"
+        ></v-text-field>
       </v-flex>
     </v-layout>
-    <v-data-table 
+    <v-data-table
       :headers="headers"
       :items="reviews"
       :search="filters"
@@ -29,7 +25,8 @@
       :custom-filter="customFilter"
     >
       <template v-slot:items="props">
-        <tr> <!-- @click="move()"차후에 이동하도록 하자. https://codepen.io/nsiggel/pen/KRdGgE-->
+        <tr>
+          <!-- @click="move()"차후에 이동하도록 하자. https://codepen.io/nsiggel/pen/KRdGgE-->
           <td class="text-xs-center">{{ props.index + 1 }}</td>
           <td class="text-xs-center">{{ props.item.rating }}</td>
           <td class="text-xs-center">{{ props.item.album.album_name }}</td>
@@ -38,7 +35,10 @@
           <td class="text-xs-center">{{ props.item.album.released_date.substring(0,10) }}</td>
           <!--<td class="text-xs-center">{{ props.item.album.label }}</td>
           <td class="text-xs-center">{{ props.item.album.agency }}</td>-->
-          <td class="text-xs-center" v-if="props.item.created_date !=null" >{{ props.item.created_date.substring(0,10) }}</td>
+          <td
+            class="text-xs-center"
+            v-if="props.item.created_date !=null"
+          >{{ props.item.created_date.substring(0,10) }}</td>
         </tr>
       </template>
     </v-data-table>
@@ -48,133 +48,162 @@
   </div>
 </template>
 <script>
-  import { mapGetters } from 'vuex' 
-  export default {
-    data () {
-      return {
-        search: '',
-        search_filter_type: 'title',
-        filter_type: [
-          'rating',
-          'title',
-          'artist',
-          'genre',
-          'released',
-          'created'
-        ],
-        filters: {
-        search: ''
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      search: "",
+      search_filter_type: "title",
+      filter_type: [
+        "rating",
+        "title",
+        "artist",
+        "genre",
+        "released",
+        "created"
+      ],
+      filters: {
+        search: ""
+      },
+      pagination: {
+        rowsPerPage: 20,
+        totalItems: ""
+      },
+      selected: [],
+      headers: [
+        { text: "순번", value: "index", align: "center", width: "1%" },
+        {
+          text: "판점",
+          //align: 'left',
+          sortable: true,
+          value: "rating",
+          align: "center",
+          width: "1%"
         },
-        pagination: {
-          rowsPerPage: 20,
-          totalItems: ''
+        { text: "title", value: "album.album_name", align: "center" },
+        { text: "artist", value: "album.artist_name", align: "center" },
+        { text: "genre", value: "album.genre", align: "center" },
+        {
+          text: "released",
+          value: "album.released_date",
+          sortable: true,
+          align: "center"
         },
-        selected: [],
-        headers: [
-          { text: '순번', value: 'index', align: 'center', width: '1%' },
-          {
-            text: '판점',
-            //align: 'left',
-            sortable: true,
-            value: 'rating',
-            align: 'center',
-            width: '1%'
-          },
-          { text: 'title', value: 'album.album_name', align: 'center' },
-          { text: 'artist', value: 'album.artist_name' , align: 'center' },
-          { text: 'genre', value: 'album.genre' , align: 'center' },
-          { text: 'released', value: 'album.released_date',sortable: true, align: 'center'  },
-          /*{ text: 'label', value: 'label', align: 'center' },
+        /*{ text: 'label', value: 'label', align: 'center' },
           { text: 'agency', value: 'agency', align: 'center' },*/
-          { text: 'created', value: 'created_date', align: 'center' }
-        ],
-        reviews: []
+        { text: "created", value: "created_date", align: "center" }
+      ],
+      reviews: []
+    };
+  },
+  methods: {
+    customFilter(items, filters, filter, headers) {
+      // Init the filter class.
+      const cf = new this.$MultiFilters(items, filters, filter, headers);
+
+      if (this.search_filter_type === "rating") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.rating == searchWord;
+          }, searchWord);
+        });
       }
-    },
-    methods: {
-      customFilter(items, filters, filter, headers) {
-        // Init the filter class.
-        const cf = new this.$MultiFilters(items, filters, filter, headers);
 
-        if(this.search_filter_type === 'rating') {
-          cf.registerFilter('search', function (searchWord, items) {
-            if (searchWord.trim() === '') return items;
-              //
-                return items.filter(item => {
-                  return item.rating.match(searchWord);
-                }, searchWord);
-              //}
+      if (this.search_filter_type === "title") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.album.album_name.includes(searchWord);
+          }, searchWord);
+        });
+      }
 
-          });
-        }
+      if (this.search_filter_type === "artist") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.album.artist_name.includes(searchWord);
+          }, searchWord);
+        });
+      }
+      if (this.search_filter_type === "genre") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.album.genre.includes(searchWord);
+          }, searchWord);
+        });
+      }
+      if (this.search_filter_type === "released") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.album.released_date.includes(searchWord);
+          }, searchWord);
+        });
+      }
+      if (this.search_filter_type === "created") {
+        cf.registerFilter("search", function(searchWord, items) {
+          if (searchWord.trim() === "") return items;
+          return items.filter(item => {
+            return item.album.created_date.includes(searchWord);
+          }, searchWord);
+        });
+      }
 
-        if(this.search_filter_type === 'title') {
-          cf.registerFilter('search', function (searchWord, items) {
-            if (searchWord.trim() === '') return items;
-              //
-                return items.filter(item => {
-                  return item.album.album_name.includes(searchWord);
-                }, searchWord);
-              //}
-
-          });
-        }
-
-        if(this.search_filter_type === 'artist') {
-          cf.registerFilter('search', function (searchWord, items) {
-            if (searchWord.trim() === '') return items;
-              //
-                return items.filter(item => {
-                  return item.album.artist_name.includes(searchWord);
-                }, searchWord);
-              //}
-
-          });
-        }
-
-
-        /*cf.registerFilter('added_by', function (added_by, items) {
+      /*cf.registerFilter('added_by', function (added_by, items) {
           if (added_by.trim() === '') return items;
           return items.filter(item => {
             return item.added_by.toLowerCase() === added_by.toLowerCase();
           }, added_by);
         });*/
-        // Its time to run all created filters.
-        // Will be executed in the order thay were defined.
-        return cf.runFilters();
-      },
-      /**
-       * Handler when user input something at the "Filter" text field.
-       */
-      filterSearch(val) {
-        this.filters = this.$MultiFilters.updateFilters(this.filters, {search: val});
-      },
-      /**
-       * Handler when user  select some author at the "Author" select.
-       */
-      filterAuthor(val) {
-        this.filters = this.$MultiFilters.updateFilters(this.filters, {added_by: val});
-      }
-    
+      // Its time to run all created filters.
+      // Will be executed in the order thay were defined.
+      return cf.runFilters();
     },
-    computed: {
-      pages () {
-        if (this.pagination.rowsPerPage == null ||
-          this.pagination.totalItems == null
-        ) return 0
+    /**
+     * Handler when user input something at the "Filter" text field.
+     */
+    filterSearch(val) {
+      this.filters = this.$MultiFilters.updateFilters(this.filters, {
+        search: val
+      });
+    },
+    /**
+     * Handler when user  select some author at the "Author" select.
+     */
+    filterAuthor(val) {
+      this.filters = this.$MultiFilters.updateFilters(this.filters, {
+        added_by: val
+      });
+    }
+  },
+  computed: {
+    pages() {
+      if (
+        this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      )
+        return 0;
 
-        return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
-      }, 
-      ...mapGetters(['isAuthenticated', 'loggedInUser'])
+      return Math.ceil(
+        this.pagination.totalItems / this.pagination.rowsPerPage
+      );
     },
-    mounted () {
-        this.$axios({
-        url: 'http://54.180.32.24:1337/graphql',
-        method: 'post',
-        data: {
-            query: `
+    ...mapGetters(["isAuthenticated", "loggedInUser"])
+  },
+  mounted() {
+    this.$axios({
+      url: "http://54.180.32.24:1337/graphql",
+      method: "post",
+      data: {
+        query:
+          `
             query {
-                reviews(where: {user: {_id: "`+this.loggedInUser._id+`"} } ) {
+                reviews(where: {user: {_id: "` +
+          this.loggedInUser._id +
+          `"} } ) {
                     _id
                     rating
                     review_title
@@ -195,13 +224,12 @@
                 }
 
             `
-        }
-        }).then((result) => { 
-            this.reviews = result.data.data.reviews
-            this.pagination.totalItems = result.data.data.reviews.length
-            console.log(this.reviews)
-        })
-        
-    }
+      }
+    }).then(result => {
+      this.reviews = result.data.data.reviews;
+      this.pagination.totalItems = result.data.data.reviews.length;
+      console.log(this.reviews);
+    });
   }
+};
 </script>
