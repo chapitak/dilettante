483247fc95ecddf14571b0788b6c58f856636ad8
O <chavitak@gmail.com>
2019-04-16 08:10:55 +0900

    아침 테스트

--- nuxt-auth/execute-deploy.sh
+++ nuxt-auth/execute-deploy.sh
@@ -3,5 +3,5 @@ sudo mkdir testmakingdir
 sudo cp -r /home/ec2-user/deploy/.nuxt/dist /home/ec2-user/dilettante/.nuxt/
 cd /home/ec2-user/dilettante
 npm install
-pm2 delete nuxt
-pm2 start strapi --no-automation --name nuxt -- start
+sudo pm2 delete nuxt
+sudo pm2 start strapi --no-automation --name nuxt -- start
