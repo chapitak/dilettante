59c5a0654e0539765d4d0d38d370de3e7ff493fc
O <chavitak@gmail.com>
2019-04-15 15:35:54 +0900

    거의 된듯

--- nuxt-auth/execute-deploy.sh
+++ nuxt-auth/execute-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir testmakingdir
-cp -r /home/ec2-user/deploy/.nuxt/dist /home/ec2-user/dilettante/.nuxt/dist
+sudo cp -r /home/ec2-user/deploy/.nuxt/dist /home/ec2-user/dilettante/.nuxt/
 cd /home/ec2-user/dilettante
 npm install
- 
+pm2 restart nuxt
