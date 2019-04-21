3977f20c99342fe9558bc0fe6e517ed2b515c5cf
chapitak <43088225+chapitak@users.noreply.github.com>
2019-04-21 15:58:24 +0900

    댓글 삭제아이콘 위치만 변경

--- nuxt-auth/components/Comment.vue
+++ nuxt-auth/components/Comment.vue
@@ -1,144 +1,124 @@
 <template>
   <div class="Comment pa-0 ma-0" v-if="props_post_id">
-    
     <div id="read-comment">
-        <v-list style="background:#fafafa">
-            <template
-            v-for="(comment, index) in comments"
-                >
-            <!-- 정보가 있는 사용자 -->    
-            <v-list-tile
-              v-if="comment.user != null"
-              :key="comment.index">
+      <v-list style="background:#fafafa">
+        <template v-for="(comment, index) in comments">
+          <!-- 정보가 있는 사용자 -->
+          <v-list-tile v-if="comment.user != null" :key="comment.index">
             <v-list-tile-content>
-                <v-list-tile-sub-title>{{comment.user.username}}</v-list-tile-sub-title>
-                    <v-layout  justify-space-around row>
-                        <div style="width:100%" pa2>{{ comment.content }} 
-                        </div>      
-                        <div v-if="isAuthenticated">
-                            <v-icon @click="deleteComment(comment._id)" small color="red" style="float:right"
-                            v-if="loggedInUser._id == comment.user._id"
-                            >close</v-icon>  
-                        </div>
-                    </v-layout>
+              <v-list-tile-sub-title>{{comment.user.username}}</v-list-tile-sub-title>
+              <v-layout justify-space-around row style="width:100%">
+                <div style="width:100%" pa2>{{ comment.content }}</div>
+                <div v-if="isAuthenticated" style="float:right">
+                  <v-icon
+                    @click="deleteComment(comment._id)"
+                    small
+                    color="red"
+                    v-if="loggedInUser._id == comment.user._id"
+                  >close</v-icon>
+                </div>
+              </v-layout>
             </v-list-tile-content>
-                <!--<div
+            <!--<div
                 :key="comment.index"
             >   {{comment.comment}}
-                </div>-->
-            </v-list-tile>
-            <!-- 탈퇴한 사용자 -->    
-            <v-list-tile
-              v-else
-              :key="comment.index">
+            </div>-->
+          </v-list-tile>
+          <!-- 탈퇴한 사용자 -->
+          <v-list-tile v-else :key="comment.index">
             <v-list-tile-content>
-                <v-list-tile-sub-title>*탈퇴한 사용자*</v-list-tile-sub-title>
-                <div style="width:100%" pa2>{{ comment.content }} 
-                
-                </div>      
+              <v-list-tile-sub-title>*탈퇴한 사용자*</v-list-tile-sub-title>
+              <div style="width:100%" pa2>{{ comment.content }}</div>
             </v-list-tile-content>
-                <!--<div
+            <!--<div
                 :key="comment.index"
             >   {{comment.comment}}
-                </div>-->
-            </v-list-tile>
+            </div>-->
+          </v-list-tile>
 
-
-
-
-
-
-
-            <v-divider :key="comment.index"
-                v-if="index+1 < comments.length"
-            ></v-divider>
-            </template>
-        </v-list>
+          <v-divider :key="comment.index" v-if="index+1 < comments.length"></v-divider>
+        </template>
+      </v-list>
     </div>
-    
+
     <div id="write-comment" v-if="isAuthenticated">
-        <!--<v-form v-model="valid">-->
-        <v-form >
-            <!-- username 들어가야되는데 prop으로 받아오겠지? --> 
-            <v-textarea
-            class="pa-0 ma-0"
-            solo
-            rows="1"
-            name="Conent Textarea"
-            label="코멘트를 작성해주세요"
-            v-model="comment"
-            required
-            >{{comment}}</v-textarea>
-            <v-btn 
-            
-            @click="sendComment()"
-            style="float:right"
-            class="pa-0 ma-0"
-            >
-            등록
-            </v-btn>
-        </v-form>
-        </div>     
-        <div v-else class="text-xs-center"> 
-            <br>
-        코멘트를 남기려면 로그인을 해주세요
+      <!--<v-form v-model="valid">-->
+      <v-form>
+        <!-- username 들어가야되는데 prop으로 받아오겠지? -->
+        <v-textarea
+          class="pa-0 ma-0"
+          solo
+          rows="1"
+          name="Conent Textarea"
+          label="코멘트를 작성해주세요"
+          v-model="comment"
+          required
+        >{{comment}}</v-textarea>
+        <v-btn @click="sendComment()" style="float:right" class="pa-0 ma-0">등록</v-btn>
+      </v-form>
     </div>
+    <div v-else class="text-xs-center">
+      <br>코멘트를 남기려면 로그인을 해주세요
     </div>
+  </div>
 </template>
 
 
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
-  export default {
-    name: 'Comment',
-    props: ['props_post_id'],
-    data() {    
-      return {
-          comment: '',
-          comments: []
-        }
-    },
-    computed: {
-    ...mapGetters(['isAuthenticated', 'loggedInUser'])
-    },
-    // watch: {
-    //     props_post_id: function () {
-    //             this.$axios({
-    //             url: 'http://54.180.32.24:1337/graphql',
-    //             method: 'post',
-    //             data: {
-    //                 query: `
-    //                 query {
-    //                     comments(where: {review: "`+this.props_post_id+`"}) { 
-    //                         content,
-    //                         _id,
-    //                         user {
-    //                         username,
-    //                         _id
-    //                     },
-                        
-    //                     }
-    //                     } 
-    //                 `
-    //             }
-    //             }).then((result) => { 
-    //             //this.commentsdatsa = result.data.data.comments 
-    //             this.comments = result.data.data.comments
-    //             console.log(this.comments)
-    //             }); 
-    //         console.log(this.comments)
-    //     }
-    // },
-    mounted() {
-                this.$axios({
-                url: 'http://54.180.32.24:1337/graphql',
-                method: 'post',
-                data: {
-                    query: `
+export default {
+  name: "Comment",
+  props: ["props_post_id"],
+  data() {
+    return {
+      comment: "",
+      comments: []
+    };
+  },
+  computed: {
+    ...mapGetters(["isAuthenticated", "loggedInUser"])
+  },
+  // watch: {
+  //     props_post_id: function () {
+  //             this.$axios({
+  //             url: 'http://54.180.32.24:1337/graphql',
+  //             method: 'post',
+  //             data: {
+  //                 query: `
+  //                 query {
+  //                     comments(where: {review: "`+this.props_post_id+`"}) {
+  //                         content,
+  //                         _id,
+  //                         user {
+  //                         username,
+  //                         _id
+  //                     },
+
+  //                     }
+  //                     }
+  //                 `
+  //             }
+  //             }).then((result) => {
+  //             //this.commentsdatsa = result.data.data.comments
+  //             this.comments = result.data.data.comments
+  //             console.log(this.comments)
+  //             });
+  //         console.log(this.comments)
+  //     }
+  // },
+  mounted() {
+    this.$axios({
+      url: "http://54.180.32.24:1337/graphql",
+      method: "post",
+      data: {
+        query:
+          `
                     query {
-                        comments(where: {review: "`+this.props_post_id+`"}) { 
+                        comments(where: {review: "` +
+          this.props_post_id +
+          `"}) { 
                             content,
                             _id,
                             user {
@@ -149,48 +129,55 @@ import { mapGetters } from 'vuex'
                         }
                         } 
                     `
-                }
-                }).then((result) => { 
-                //this.commentsdatsa = result.data.data.comments 
-                this.comments = result.data.data.comments
-                console.log(this.comments)
-                }); 
-            console.log(this.comments)
-    },
-    
-    methods: {
-        sendComment() {
-        if(this.comment == '') {
-            alert("내용을 먼저 작성해주세요")
-            return
-        }
-        this.$axios.post(`http://jeongkyo.kim:1337/comments/`, {
-            //post_id: this.props_post_id,
-            register_id: this.loggedInUser._id,
-            content: this.comment,
-            review: this.props_post_id,
-            user: this.loggedInUser._id
+      }
+    }).then(result => {
+      //this.commentsdatsa = result.data.data.comments
+      this.comments = result.data.data.comments;
+      console.log(this.comments);
+    });
+    console.log(this.comments);
+  },
+
+  methods: {
+    sendComment() {
+      if (this.comment == "") {
+        alert("내용을 먼저 작성해주세요");
+        return;
+      }
+      this.$axios
+        .post(`http://jeongkyo.kim:1337/comments/`, {
+          //post_id: this.props_post_id,
+          register_id: this.loggedInUser._id,
+          content: this.comment,
+          review: this.props_post_id,
+          user: this.loggedInUser._id
         })
         .then(response => {
-            // Handle success.
-            console.log(
-            'Well done, your post has been successfully created: ',
+          // Handle success.
+          console.log(
+            "Well done, your post has been successfully created: ",
             response.data
-            );
+          );
         })
         .catch(error => {
-            // Handle error.
-            console.log('An error occurred:', error);
+          // Handle error.
+          console.log("An error occurred:", error);
         });
-        this.comments.push({content: this.comment, user: {username: this.loggedInUser.username, _id: this.loggedInUser._id}});
-        this.comment = '';
-        },
-        deleteComment(cid) {
-            console.log(cid)
-            var r = confirm("정말 삭제하시곘습니까?")
-            if(r==true) {
-                this.$axios.delete(`http://jeongkyo.kim:1337/comments/` + cid)
-                .then/*(response => {
+      this.comments.push({
+        content: this.comment,
+        user: {
+          username: this.loggedInUser.username,
+          _id: this.loggedInUser._id
+        }
+      });
+      this.comment = "";
+    },
+    deleteComment(cid) {
+      console.log(cid);
+      var r = confirm("정말 삭제하시곘습니까?");
+      if (r == true) {
+        this.$axios.delete(`http://jeongkyo.kim:1337/comments/` + cid)
+          .then; /*(response => {
                     // Handle success.
                     console.log(
                     'Well done, your post has been successfully updated: ',
@@ -202,31 +189,33 @@ import { mapGetters } from 'vuex'
                     // Handle error.
                     console.log('An error occurred:', error);
                 });*/
-                const idx = this.comments.findIndex(function(item) {return item._id === cid}) // findIndex = find + indexOf 
-                    if (idx > -1) this.comments.splice(idx, 1)
-            } 
-        }
+        const idx = this.comments.findIndex(function(item) {
+          return item._id === cid;
+        }); // findIndex = find + indexOf
+        if (idx > -1) this.comments.splice(idx, 1);
+      }
     }
   }
+};
 </script>
 
 <style scoped>
 .Comment {
-    box-sizing: border-box;
-    min-width: 200px;
-    max-width: 980px;
-    margin: 0 auto;
-    padding: 45px;
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
 }
 @media (max-width: 767px) {
-    .Comment {
-        padding: 15px;
-    }
+  .Comment {
+    padding: 15px;
+  }
 }
 </style>
 
 <style>
 .Comment .v-list__tile {
-    height: auto;
+  height: auto;
 }
 </style>
--- nuxt-auth/components/ViewSingleReview.vue
+++ nuxt-auth/components/ViewSingleReview.vue
@@ -1,80 +1,91 @@
 <template>
-    <div class="ViewSingleReview"  v-if="reviewId">
-      
-        <br>
-        <template>
-        <v-layout justify-center>
-          <v-flex md8>
-            <v-card>
-              <div class="pa-2"> <span>&nbsp;&nbsp;&nbsp;<b>{{reviewer_name}}</b></span></div>
-              <v-parallax :src="parallax_src" height="120"></v-parallax>
-              <!--<v-img
+  <div class="ViewSingleReview" v-if="reviewId">
+    <br>
+    <template>
+      <v-layout justify-center>
+        <v-flex md8>
+          <v-card>
+            <div class="pa-2">
+              <span>
+                &nbsp;&nbsp;&nbsp;
+                <b>{{reviewer_name}}</b>
+              </span>
+            </div>
+            <v-parallax :src="parallax_src" height="160"></v-parallax>
+            <!--<v-img
                 src="http://s3.ap-northeast-2.amazonaws.com/dilettante/4275.jpg"
                 aspect-ratio="2.75"
-              ></v-img>-->
+            ></v-img>-->
 
-              <v-card-title primary-title>
-                <div style="width:100%">
-                  <div>{{artist_name}}</div>
-                  <h3 class="headline mb-0">{{album_name}}</h3>
-                  <span>{{genre}} ☆{{rating}} </span><span style="float:right">{{released_date.substring(0,10)}}</span>
-                  <br>
-                  <div class="pt-2"> {{ review_text }} </div>
-                </div>
-              </v-card-title>
+            <v-card-title primary-title>
+              <div style="width:100%">
+                <div>{{artist_name}}</div>
+                <h3 class="headline mb-0">{{album_name}}</h3>
+                <span>{{genre}} ☆{{rating}}</span>
+                <span style="float:right">{{released_date.substring(0,10)}}</span>
+                <br>
+                <div class="pt-2">{{ review_text }}</div>
+              </div>
+            </v-card-title>
 
-              <v-card-actions class="justify-center">
-                <!-- <v-btn flat color="orange"><v-icon light>audiotrack</v-icon> 6</v-btn> -->
-                <v-btn style="width:100%;" flat color="orange" @click="move(reviewId)"><v-icon light>comment</v-icon> &nbsp;more</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </template>
-    </div>
+            <v-card-actions class="justify-center">
+              <!-- <v-btn flat color="orange"><v-icon light>audiotrack</v-icon> 6</v-btn> -->
+              <v-btn style="width:100%;" flat color="orange" @click="move(reviewId)">
+                <v-icon light>comment</v-icon>&nbsp;more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ViewSingleReview',
-  props: ['reviewId'],
+  name: "ViewSingleReview",
+  props: ["reviewId"],
   data() {
     return {
-        artist_name: "언니네 이발관",
-        album_name: "후일담",
-        review_text: '5집보다 좋은 앨범이다. 동의하지 않는 사람과 논쟁하지 않겠다. 그는 논쟁할 가치가 없는 사람이기 때문이다.',
-        released_date: "2001-01-23",
-        rating: "4.0",
-        genre: "Rock",
-        parallax_src: "",
-        reviewer_name: ""
-    }
+      artist_name: "언니네 이발관",
+      album_name: "후일담",
+      review_text:
+        "5집보다 좋은 앨범이다. 동의하지 않는 사람과 논쟁하지 않겠다. 그는 논쟁할 가치가 없는 사람이기 때문이다.",
+      released_date: "2001-01-23",
+      rating: "4.0",
+      genre: "Rock",
+      parallax_src: "",
+      reviewer_name: ""
+    };
   },
   methods: {
     move(target) {
-        this.$router.push("/album/reviews/" + target)
-    },
+      this.$router.push("/album/reviews/" + target);
+    }
   },
   mounted() {
-    this.$axios.get('http://jeongkyo.kim:1337/reviews/' + this.reviewId)
+    this.$axios
+      .get("http://jeongkyo.kim:1337/reviews/" + this.reviewId)
       .then(response => {
         // Handle success.
-        console.log(
-          response.data
-        );
+        console.log(response.data);
 
-        this.artist_name = response.data.album.artist_name
-        this.album_name = response.data.album.album_name
-        this.review_text = response.data.review_text
-        this.released_date = response.data.album.released_date
-        this.genre = response.data.album.genre
-        this.rating = response.data.rating
-        this.reviewer_name = response.data.user.username
-        this.reviewer_name = response.data.user.username
-        this.parallax_src = "http://s3.ap-northeast-2.amazonaws.com/dilettante/" + response.data.album.album_id + ".jpg"
-    })
+        this.artist_name = response.data.album.artist_name;
+        this.album_name = response.data.album.album_name;
+        this.review_text = response.data.review_text;
+        this.released_date = response.data.album.released_date;
+        this.genre = response.data.album.genre;
+        this.rating = response.data.rating;
+        this.reviewer_name = response.data.user.username;
+        this.reviewer_name = response.data.user.username;
+        this.parallax_src =
+          "http://s3.ap-northeast-2.amazonaws.com/dilettante/" +
+          response.data.album.album_id +
+          ".jpg";
+      });
   },
-  watch: {/*
+  watch: {
+    /*
     reviewId: function() {
         this.$axios.get('http://jeongkyo.kim:1337/reviews/' + this.reviewId)
         .then(response => {
@@ -87,5 +98,5 @@ export default {
       })
     }*/
   }
-}
+};
 </script>
