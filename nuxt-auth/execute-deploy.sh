733aa013b2083e18749380f35229b4d505af755c
O <chavitak@gmail.com>
2019-04-29 11:02:54 +0900

    배포 손봄

--- nuxt-auth/execute-deploy.sh
+++ nuxt-auth/execute-deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #sudo mkdir testmakingdir
-sudo cp -r /home/ec2-user/deploy/.nuxt/* /home/ec2-user/dilettante/.nuxt/*
+sudo cp -r /home/ec2-user/deploy/* /home/ec2-user/dilettante/
 cd /home/ec2-user/dilettante
 npm install
 
 sudo su - ec2-user -c "cd dilettante; pm2 delete nuxt; pm2 start npm --name \"nuxt\" -- start "
 # pm2 delete nuxt
 # pm2 start 
+
+
+#  sudo cp -r /home/ec2-user/deploy/* /home/ec2-user/dilettante/
